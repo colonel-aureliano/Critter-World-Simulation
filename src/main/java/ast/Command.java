01e65e1303edcc5d41e103d8c9b7967a725579c8
yq54 <11708+yq54@users.noreply.github.coecis.cornell.edu>
2021-10-23 22:28:27 -0400

    command + factor

--- src/main/java/ast/Command.java
+++ src/main/java/ast/Command.java
@@ -1,6 +1,20 @@
 package ast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Command extends AbstractNode{
+
+    private List<Node> coms;
+
+    public Command(Node n) {
+        coms = new ArrayList<Node>();
+        coms.add(n);
+    }
+
+    public void add(Node n) {
+        coms.add(n);
+    }
 
     @Override
     public String toString() {
--- src/main/java/ast/Factor.java
+++ src/main/java/ast/Factor.java
@@ -1,10 +1,37 @@
 package ast;
 
-public class Factor extends Term{
-    private int n;
+public class Factor extends Expr {
+    private int value;
+    private Expr expression;
+    private Operator operator;
 
-    public Factor(String n){
-        System.out.println("Factor class storing: "+n);
-        this.n=Integer.parseInt(n);
+    public Factor (int v) {
+        value = v;
+    }
+
+    public Factor (Operator op, int v) {
+        operator = op;
+        value = v;
+    }
+
+    public Factor (Operator op, Expr e) {
+        operator = op;
+        expression = e;
+    }
+
+    public enum Operator {
+        NEGATIVE,
+        MEM;
+    }
+
+
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    public boolean classInv() {
+        return false;
     }
 }
