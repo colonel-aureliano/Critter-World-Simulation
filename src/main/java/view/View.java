47f7a8afa3a9ba21c854719da6b73c54edaac83c
yq54 <11708+yq54@users.noreply.github.coecis.cornell.edu>
2021-12-02 16:26:21 -0500

    12.2

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,25 +1,37 @@
 package view;
 
+import controller.Controller;
+import controller.ControllerFactory;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import model.ReadOnlyWorld;
 
-import javax.swing.*;
-import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
 public class View extends Application {
 
+    private final Controller controller = ControllerFactory.getConsoleController();
+    Alert a = new Alert(Alert.AlertType.WARNING);
+    GraphicsContext gc;
+    ReadOnlyWorld w;
+
     @FXML
     private Button LoadWorld;
     @FXML
     private Button LoadCritter;
-    @FXML
-    private Label label1;
     @FXML
     private TextField nCritters;
     @FXML
@@ -29,13 +41,9 @@ public class View extends Application {
     @FXML
     private Button Stop;
     @FXML
-    private Label label2;
-    @FXML
-    private JTextField RunRate;
+    private TextField RunRate;
     @FXML
     private Canvas canvas;
-
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -48,6 +56,7 @@ public class View extends Application {
             }
             final Parent node = FXMLLoader.load(r);
             final Scene scene = new Scene(node);
+
             primaryStage.setScene(scene);
             primaryStage.sizeToScene();
             primaryStage.show();
@@ -58,4 +67,61 @@ public class View extends Application {
             try { stop(); } catch (final Exception e2) {}
         }
     }
+
+    private void drawHex() {
+        gc = canvas.getGraphicsContext2D();
+        w = controller.getReadOnlyWorld();
+        gc.fillPolygon();
+        gc.fillRect(0,0,canvas.getHeight(), canvas.getHeight());
+    }
+
+    @FXML
+    private void loadWorld(final ActionEvent ae) {
+        System.out.println("World button pressed");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open World File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+        if (!controller.loadWorld(selectedFile.getAbsolutePath(), false, false)) {
+            a.setContentText("Load world failed.");
+            a.show();
+        }
+        drawHex();
+    }
+
+    @FXML
+    private void loadCritter(final ActionEvent ae) {
+        System.out.println("Critter button pressed");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Critter File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        Stage stage = new Stage();
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        String nStr = nCritters.getText();
+        if (!nStr.matches("[0-9]+") ||
+                !controller.loadCritters(selectedFile.getName(), Integer.valueOf(nStr))) {
+            a.setContentText("Load critter failed");
+            a.show();
+        }
+        drawHex();
+    }
+
+    @FXML
+    private void playOnce(final ActionEvent ae) {
+        controller.advanceTime(1);
+    }
+
+    @FXML
+    private void play(final ActionEvent ae) {
+
+    }
+
+    @FXML
+    private void stop(final ActionEvent ae) {
+
+    }
+
+
 }
